25dadf60b405ace8449cd16c9a522d5291a6ee7f
Kai Fleischman <kfleischman@duosecurity.com>
2020-06-30 18:41:58 -0500

    Moved final code from LogSyncBase into app.py, deleted duo_log_sync_base.py

--- duologsync/app.py
+++ duologsync/app.py
@@ -1,11 +1,16 @@
 import argparse
 import asyncio
-import duologsync.duo_log_sync_base
 
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
 from duologsync.config_generator import ConfigGenerator
-from duologsync.util import create_admin
+from duologsync.util import create_admin, create_writer, get_last_offset_read
+from duologsync.consumer.adminaction_consumer import AdminactionConsumer
+from duologsync.producer.adminaction_producer import AdminactionProducer
+from duologsync.consumer.authlog_consumer import AuthlogConsumer
+from duologsync.producer.authlog_producer import AuthlogProducer
+from duologsync.consumer.telephony_consumer import TelephonyConsumer
+from duologsync.producer.telephony_producer import TelephonyProducer
 
 def main():
     """
@@ -22,6 +27,58 @@ def main():
 
     # namedtuple containing important variables used through DuoLogSync
     g_vars = create_global_tuple(args.ConfigPath)
+
+    #####################
+
+    writer = g_vars.event_loop.run_until_complete(
+        create_writer(g_vars.config, g_vars.event_loop)
+    )
+
+    # Enable endpoints based on user selection
+    tasks = []
+    enabled_endpoints = g_vars.config['logs']['endpoints']['enabled']
+    for endpoint in enabled_endpoints:
+        new_queue = asyncio.Queue(loop=g_vars.event_loop)
+        producer = consumer = None
+
+        # Populate last_offset_read for each enabled endpoint
+        if g_vars.config['recoverFromCheckpoint']['enabled']:
+            g_vars.last_offset_read[
+                f"{endpoint}_checkpoint_data.txt"
+            ] = get_last_offset_read(
+                g_vars.config['logs']['checkpointDir'],
+                endpoint
+            )
+
+        if endpoint == 'auth':
+            producer = AuthlogProducer(g_vars.config, g_vars.last_offset_read,
+                                       new_queue, g_vars)
+            consumer = AuthlogConsumer(g_vars.config, g_vars.last_offset_read,
+                                       new_queue, writer)
+        elif endpoint == "telephony":
+            producer = TelephonyProducer(g_vars.config, g_vars.last_offset_read,
+                                         new_queue, g_vars)
+            consumer = TelephonyConsumer(g_vars.config, g_vars.last_offset_read,
+                                         new_queue, writer)
+        elif endpoint == "adminaction":
+            producer = AdminactionProducer(g_vars.config,
+                                           g_vars.last_offset_read,
+                                           new_queue, g_vars)
+            consumer = AdminactionConsumer(g_vars.config,
+                                           g_vars.last_offset_read,
+                                           new_queue, writer)
+        else:
+            logging.info("%s is not a recognized endpoint", endpoint)
+            del new_queue
+            continue
+
+        tasks.append(asyncio.ensure_future(producer.produce()))
+        tasks.append(asyncio.ensure_future(consumer.consume()))
+
+    g_vars.event_loop.run_until_complete(asyncio.gather(*tasks))
+    g_vars.event_loop.close()
+
+    #####################
 
     logger = duologsync.duo_log_sync_base.LogSyncBase()
     logger.start(g_vars)
