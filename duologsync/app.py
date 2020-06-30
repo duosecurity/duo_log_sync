37e72bde8fa33d7ba4217e79cfe4c450a7c2c172
Kai Fleischman <kfleischman@duosecurity.com>
2020-06-30 18:26:08 -0500

    Moved instantiation of g_vars to function create_global_tuple

--- duologsync/app.py
+++ duologsync/app.py
@@ -8,11 +8,34 @@ from duologsync.config_generator import ConfigGenerator
 from duologsync.util import create_admin
 
 def main():
+    """
+    Kicks off DuoLogSync by setting important variables, creating and running
+    a Producer-Consumer pair for each log-type defined in a config file passed
+    to the program.
+    """
+
     arg_parser = argparse.ArgumentParser(prog='duologsync',
                                          description="Path to config file")
     arg_parser.add_argument('ConfigPath', metavar='config-path', type=str,
                             help='Config to start application')
     args = arg_parser.parse_args()
+
+    # namedtuple containing important variables used through DuoLogSync
+    g_vars = create_global_tuple(args.ConfigPath)
+
+    logger = duologsync.duo_log_sync_base.LogSyncBase()
+    logger.start(g_vars)
+
+def create_global_tuple(config_path):
+    """
+    Initialize important variables used throughout DuoLogSync and return a
+    namedtuple which contains them and allows accessing the variables by name.
+
+    @param config_path  Location of a config file which is used to create a
+                        config dictionary object.
+
+    @return a namedtuple with important variables used throughout DuoLogSync
+    """
 
     # Create a tuple containing global variables that may be indexed by name
     g_vars = namedtuple(
@@ -20,7 +43,7 @@ def main():
         ['admin', 'config', 'event_loop', 'executor', 'last_offset_read'])
 
     # Dictionary populated with values from the config file passed to DuoLogSync
-    g_vars.config = ConfigGenerator().get_config(args.ConfigPath)
+    g_vars.config = ConfigGenerator().get_config(config_path)
 
     # Object that allows for interaction with Duo APIs to fetch logs / data
     g_vars.admin = create_admin(
@@ -29,8 +52,7 @@ def main():
         g_vars.config['duoclient']['host']
     )
 
-    # The core to asyncio applications. Event loops run asynchronous tasks and
-    # call-backs, perform network IO operations and run subprocesses
+    # Object that can run asynchronous tasks, call-backs and subprocesses
     g_vars.event_loop = asyncio.get_event_loop()
 
     # Allocate an execution environment of 3 threads for high latency tasks
@@ -39,5 +61,4 @@ def main():
     # Dictionary for storing the latest timestamp received for each log type
     g_vars.last_offset_read = {}
 
-    logger = duologsync.duo_log_sync_base.LogSyncBase()
-    logger.start(g_vars)
+    return g_vars
