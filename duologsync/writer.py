5b6bb8f76d5c3a8ddb1a0e599df1c13f5dffe245
Kai Fleischman <kfleischman@duosecurity.com>
2020-07-28 14:17:49 -0500

    Bubbled up exceptions to create_writer

--- duologsync/writer.py
+++ duologsync/writer.py
@@ -5,7 +5,6 @@ Definition of the Writer class
 import asyncio
 import ssl
 import logging
-import functools
 from socket import gaierror
 from duologsync.program import Program
 
@@ -29,14 +28,14 @@ class DatagramProtocol(asyncio.DatagramProtocol):
 
         if exc:
             shutdown_reason = (
-                f"DuoLogSync: UDP connection with host-{self.host} and port-"
-                f"{self.port} was closed for the following reason [{exc}]"
+                f"UDP connection with host-{self.host} and port-{self.port}"
+                f"was closed for the following reason [{exc}]"
             )
 
         else:
             shutdown_reason = (
-                f"DuoLogSync: UDP connection with host-{self.host} and port-"
-                f"{self.port} was closed"
+                f"UDP connection with host-{self.host} and port-{self.port} "
+                "was closed"
             )
 
         Program.initiate_shutdown(shutdown_reason)
@@ -53,7 +52,7 @@ class Writer:
 
         # Create the actual writer
         self.writer = asyncio.get_event_loop().run_until_complete(
-            self.create_connection(
+            self.create_writer(
                 transport_settings['host'],
                 transport_settings['port'],
                 transport_settings.get('certFilepath')
@@ -74,7 +73,7 @@ class Writer:
             self.writer.write(data)
             await self.writer.drain()
 
-    async def create_connection(self, host, port, cert_filepath):
+    async def create_writer(self, host, port, cert_filepath):
         """
         Wrapper for functions to create TCP or UDP connections.
 
@@ -85,79 +84,34 @@ class Writer:
         @return a 'writer' object for writing data over the connection made
         """
 
-        writer_func = None
-
-        # Which component of the returned writer tuple is actually the writer
-        writer_index = 1
-
-        # UDP connection is very different from creating a TCP connection
-        # because UDP is unsorted and not buffered
-        if self.protocol == 'UDP':
-            writer_index = 0
-            writer_func = functools.partial(
-                asyncio.get_event_loop().create_datagram_endpoint,
-                lambda: DatagramProtocol(host, port), remote_addr=(host, port))
-
-        # TCP connection (either over SSL or not)
-        else:
-            ssl_context = None
-
-            if self.protocol == 'TCPSSL':
-                ssl_context = Writer.get_ssl_context(cert_filepath)
-
-            writer_func = functools.partial(
-                asyncio.open_connection, host, port, ssl=ssl_context)
-
-        writer_tuple = await Writer.create_writer(writer_func, host, port)
-        return writer_tuple[writer_index]
-
-    @staticmethod
-    async def get_ssl_context(cert_filepath):
-        """
-        Wrapper for creating an ssl context.
-
-        @param cert_filepath    Location of the certificate to use for SSL
-
-        @return an ssl context
-        """
-
-        try:
-            ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH,
-                                                     cafile=cert_filepath)
-
-        except FileNotFoundError:
-            Program.initiate_shutdown(
-                f"The certificate file {cert_filepath} could not be opened.")
-            Program.log('DuoLogSync: Make sure the filepath for SSL cert '
-                        'file is correct.', logging.ERROR)
-            return None
-
-        else:
-            return ssl_context
-
-    @staticmethod
-    async def create_writer(writer_function, host, port):
-        """
-        Wrapper around the asyncio.open_connection function with exception
-        handling for creating a network connection to host and port.
-
-        @param writer_function  Function used for creating a network connection
-        @param host             Hostname of the network connection to establish
-        @param port             Port of the network connection to establish
-
-        @return an asyncio open_connection tuple containing an object used to
-                write data over an established network connection
-        """
-
-        shutdown_reason = None
         Program.log(f"DuoLogSync: Opening connection to {host}:{port}",
                     logging.INFO)
 
+        # Message to be logged if an error occurs in this function
+        help_message = (
+            f"DuoLogSync: check that host-{host} and port-{port} "
+            "are correct in the config file")
+        writer = None
+
         try:
-            writer_tuple = await asyncio.wait_for(
-                writer_function(),
-                timeout=60
-            )
+            if self.protocol == 'UDP':
+                writer = await Writer.create_udp_writer(host, port)
+
+            elif self.protocol == 'TCPSSL':
+                ssl_context = ssl.create_default_context(
+                    ssl.Purpose.SERVER_AUTH, cafile=cert_filepath)
+
+                writer = await Writer.create_tcp_writer(host, port, ssl_context)
+
+            elif self.protocol == 'TCP':
+                writer = await Writer.create_tcp_writer(host, port)
+
+        # Failed to open the certificate file
+        except FileNotFoundError:
+            shutdown_reason = f"{cert_filepath} could not be opened."
+            help_message = (
+                'DuoLogSync: Make sure the filepath for SSL cert file is '
+                'correct.')
 
         # Couldn't establish a connection within 60 seconds
         except asyncio.TimeoutError:
@@ -173,9 +127,52 @@ class Writer:
 
         # An error did not occur and the writer was successfully created
         else:
-            return writer_tuple
+            return writer
 
         Program.initiate_shutdown(shutdown_reason)
-        Program.log(f"DuoLogSync: check that host-{host} and port-{port} are "
-                    "correct in the config file")
+        Program.log(help_message, logging.ERROR)
         return None
+
+    @staticmethod
+    async def create_udp_writer(host, port):
+        """
+        Wrapper around the create_datagram_endpoint function for creating
+        UDP connections.
+
+        @param host Hostname of the network conneciton to establish
+        @param port Port of the network connection to establish
+
+        @return an asyncio object used to write data over a network connection
+                using UDP
+        """
+
+        writer, _ = await asyncio.wait_for(
+            asyncio.get_event_loop().create_datagram_endpoint(
+                lambda: DatagramProtocol(host, port),
+                remote_addr=(host, port)
+            ),
+            timeout=60
+        )
+
+        return writer
+
+    @staticmethod
+    async def create_tcp_writer(host, port, ssl_context=None):
+        """
+        Wrapper around the asyncio.open_connection function with exception
+        handling for creating a network connection to host and port.
+
+        @param host     Hostname of the network connection to establish
+        @param port     Port of the network connection to establish
+        @ssl_context    Used for creating TCP over SSL connections
+
+        @return an asyncio object used to write data over a network connection
+                using TCP
+        """
+
+        _, writer = await asyncio.wait_for(
+            asyncio.open_connection(host, port, ssl=ssl_context),
+            timeout=60
+        )
+
+        return writer
