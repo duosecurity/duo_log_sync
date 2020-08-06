205ab037b6d76e8190875376b644f12a641d78c8
Kai Fleischman <kfleischman@duosecurity.com>
2020-08-06 14:23:59 -0500

    Updated config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -55,111 +55,119 @@ class TestConfig(TestCase):
         with self.assertRaises(ValueError):
             Config.get_value(['house_key', 'car_key'])
 
-    def test_get_enabled_endpoints(self):
-        config = {'logs': {'endpoints': {'enabled': ['one', 'two', 'three']}}}
-
-        Config.set_config(config)
-        enabled_endpoints = Config.get_enabled_endpoints()
-
-        self.assertEqual(enabled_endpoints, ['one', 'two', 'three'])
-
-    def test_get_polling_duration(self):
-        config = {'logs': {'polling': {'duration': 1234}}}
-
-        Config.set_config(config)
-        polling_duration = Config.get_polling_duration()
-
-        self.assertEqual(polling_duration, 1234)
-
-    def test_get_checkpoint_directory(self):
-        config = {'logs': {'checkpointDir': '/tmp'}}
-
-        Config.set_config(config)
-        checkpoint_directory = Config.get_checkpoint_directory()
-
-        self.assertEqual(checkpoint_directory, '/tmp')
-
-    def test_get_ikey(self):
-        config = {'duoclient': {'ikey': 'Integration Key'}}
-
-        Config.set_config(config)
-        ikey = Config.get_ikey()
-
-        self.assertEqual(ikey, 'Integration Key')
-
-    def test_get_skey(self):
-        config = {'duoclient': {'skey': 'Secret Key'}}
-
-        Config.set_config(config)
-        skey = Config.get_skey()
-
-        self.assertEqual(skey, 'Secret Key')
-
-    def test_get_host(self):
-        config = {'duoclient': {'host': 'duosecurity.com'}}
-
-        Config.set_config(config)
-        host = Config.get_host()
-
-        self.assertEqual(host, 'duosecurity.com')
-
-    def test_get_recover_log_offset(self):
-        config = {'recoverFromCheckpoint': {'enabled': True}}
-
-        Config.set_config(config)
-        recover_log_offset = Config.get_recover_log_offset()
-
-        self.assertEqual(recover_log_offset, True)
-
     def test_get_log_filepath(self):
-        config = {'logs': {'logFilepath': '/tmp/duologsync.log'}}
+        config = {'dls_settings': {'log_filepath': '/dev/null'}}
 
         Config.set_config(config)
         log_filepath = Config.get_log_filepath()
 
-        self.assertEqual(log_filepath, '/tmp/duologsync.log')
+        self.assertEqual(log_filepath, '/dev/null')
 
     def test_get_log_format(self):
-        config = {'logs': {'log_format': 'CEF'}}
+        config = {'dls_settings': {'log_format': 'JSON'}}
 
         Config.set_config(config)
         log_format = Config.get_log_format()
 
-        self.assertEqual(log_format, 'CEF')
+        self.assertEqual(log_format, 'JSON')
+
+    def test_get_checkpointing_enabled(self):
+        config = {'dls_settings': {'checkpointing': {'enabled': False}}}
+
+        Config.set_config(config)
+        checkpointing_enabled = Config.get_checkpointing_enabled()
+
+        self.assertEqual(checkpointing_enabled, False)
+
+    def test_get_checkpoint_dir(self):
+        config = {'dls_settings': {'checkpointing': {'checkpoint_dir': '/tmp'}}}
+
+        Config.set_config(config)
+        checkpoint_dir = Config.get_checkpoint_dir()
+
+        self.assertEqual(checkpoint_dir, '/tmp')
+
+    def test_get_servers(self):
+        config = {'servers': ['item1', 'item2']}
+
+        Config.set_config(config)
+        servers = Config.get_servers()
+
+        self.assertEqual(servers, ['item1', 'item2'])
+
+    def test_get_accounts(self):
+        config = {'accounts': ['personal', 'private']}
+
+        Config.set_config(config)
+        accounts = Config.get_accounts()
+
+        self.assertEqual(accounts, ['personal', 'private'])
 
     def test_create_config_normal(self):
         config_filepath = 'tests/resources/config_files/standard.yml'
         correct_config = {
-            'duoclient': {
-                'skey': 'Shhh, this is a secret...',
-                'ikey': 'BUNCH OF RANDOM CHARACTERS ALL UPPER CASE',
-                'host': 'duosecurity.com'
-            },
-            'logs': {
-                'logFilepath': '/tmp/duologsync.log',
-                'endpoints': {
-                    'enabled': ['auth', 'adminaction', 'telephony']
-                },
-                'polling': {
-                    'duration': 120,
-                    'daysinpast': 180
-                },
-                'checkpointDir': '/tmp',
+            'version': '1.0.0',
+            'dls_settings': {
+                'log_filepath': '/tmp/duologsync.log',
                 'log_format': 'JSON',
-                'offset': None
+                'api': {
+                    'offset': 180,
+                    'timeout': 120
+                },
+                'checkpointing': {
+                    'enabled': False,
+                    'checkpoint_dir': '/tmp/dls_checkpoints'
+                }
             },
-            'transport': {
-                'protocol': 'TCP',
-                'host': 'localhost',
-                'port': 8888
-            },
-            'recoverFromCheckpoint': {
-                'enabled': False
-            }
+            'servers': [
+                {
+                    'id': 'main server',
+                    'hostname': 'mysiem.com',
+                    'port': 8888,
+                    'protocol': 'TCPSSL',
+                    'cert_filepath': 'cert.crt'
+                },
+                {
+                    'id': 'backup',
+                    'hostname': 'safesiem.org',
+                    'port': 13031,
+                    'protocol': 'UDP'
+                }
+            ],
+            'accounts': [
+                {
+                    'ikey': 'AAA101020K12K1K23',
+                    'skey': 'jyJKYAGJKAYGDKJgyJygFUg9F9gyFuo9',
+                    'hostname': 'api-test.first.duosecurity.com',
+                    'endpoint_server_mappings': [
+                        {
+                            'endpoints': ['adminaction', 'auth'],
+                            'servers': ['main server', 'backup']
+                        },
+                        {
+                            'endpoints': 'telephony',
+                            'servers': 'backup'
+                        }
+                    ],
+                    'is_msp': True,
+                    'block_list': []
+                },
+                {
+                    'ikey': 'DLFKSJFKJLALFK4444',
+                    'skey': 'jln;89NO89898*(P',
+                    'hostname': 'second.duosecurity.com',
+                    'endpoint_server_mappings': [
+                        {
+                            'endpoints': 'auth',
+                            'servers': 'backup'
+                        }
+                    ]
+                }
+            ]
         }
 
         config = Config.create_config(config_filepath)
-        config['logs']['offset'] = None
+        config['dls_settings']['api']['offset'] = 180
 
         self.assertEqual(correct_config, config)
 
@@ -188,27 +196,14 @@ class TestConfig(TestCase):
         
         mock_initiate_shutdown.assert_called_once()
 
-    def test_create_config_with_missing_optional_fields(self):
-        config_filepath = 'tests/resources/config_files/no_optional_fields.yml'
-
-        config = Config.create_config(config_filepath)
-
-        self.assertNotEqual(config['logs']['logFilepath'], None)
-        self.assertNotEqual(config['logs']['polling']['duration'], None)
-        self.assertNotEqual(config['logs']['polling']['daysinpast'], None)
-        self.assertNotEqual(config['logs']['checkpointDir'], None)
-        self.assertNotEqual(config['logs']['log_format'], None)
-        self.assertNotEqual(config['recoverFromCheckpoint']['enabled'], None)
-
-    def test_create_config_with_polling_too_low(self):
+    @patch('duologsync.program.Program.initiate_shutdown',
+           side_effect=running_is_false)
+    def test_create_config_with_polling_too_low(self, mock_initiate_shutdown):
         config_filepath = 'tests/resources/config_files/polling_too_low.yml'
 
         config = Config.create_config(config_filepath)
 
-        print(config)
-
-        self.assertEqual(config['logs']['polling']['duration'], 120)
-
+        mock_initiate_shutdown.assert_called_once()
 
     def test_get_value_from_keys_normal(self):
         dictionary = {'level_one': '2FA',
@@ -230,21 +225,3 @@ class TestConfig(TestCase):
 
         self.assertEqual(value_one, None)
         self.assertEqual(value_two, None)
-
-    def test_set_value_from_keys(self):
-        dictionary = {'Computer Science': {'Fun?': False}}
-
-        dictionary = Config.set_value_from_keys(dictionary,
-                                                ('Computer Science', 'Fun?'),
-                                                True)
-
-        self.assertEqual(dictionary['Computer Science']['Fun?'], True)
-
-    def test_set_value_from_keys_bad_keys(self):
-        dictionary = {'Ice Cream': {'Favorite': 'Birthday Cake'}}
-
-        dictionary = Config.set_value_from_keys(dictionary,
-                                                ('Ice Cream', 'Favourite'),
-                                                'Coffee')
-
-        self.assertEqual(dictionary['Ice Cream']['Favorite'], 'Birthday Cake')
